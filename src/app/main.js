206b8324d4a7619ee0d2b75ab00a11503756dd78
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-09-22 17:01:19 -0400

    Update main.js

--- src/app/main.js
+++ src/app/main.js
@@ -1,15 +1,54 @@
-// ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        http://*/*
-// @grant        none
-// ==/UserScript==
+/*
+==UserScript==
+Main
+---------------------------------
+@name         R2010's Extensions
+@icon         https://angular.io/assets/images/logos/angular/angular.svg
+@namespace    *://scratch.mit.edu/projects/*
+@version      1.0.0 - Pre-release
+@description  try to take over the world!
+@author       redstonelasher2010 on Scratch
+---------------------------------
+Matches
+---------------------------------
+@match        https://scratch.mit.edu/projects/*
+@match        https://github.io/
+@match        https://github.com/
+---------------------------------
 
+Grants
+---------------------------------
+@grant        https://github.com/
+@grant        https://github.io/
+---------------------------------
+
+Requirements
+---------------------------------
+@require      https://github.com/
+@require      https://github.io/
+@require      https://angular.io/
+---------------------------------
+
+Resources
+---------------------------------
+@resource     https://angular.io/
+@resource     https://scratch.mit.edu
+---------------------------------
+==/UserScript==
+*/
+'use strict';
 (function() {
-    'use strict';
 
-    // Your code here...
+const compile = function toSwift(){ 
+    // // this function should compile code in here to swift \\ \\
+    
+}
+/*
+Variables
+----------------------------
+*/
+var nil = null;
+var Scope = 'object.scope';
+var x = '$' + nil .'compileToSwift.' + Scope + '{(item=[strictmode] >> type=[in] >> in(([function]))';
+console.log(x);
 })();
