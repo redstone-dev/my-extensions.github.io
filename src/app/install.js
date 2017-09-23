7de22e98c413266c934a69ef340dbecd7adeef1c
redstone2010 <31410740+redstone2010@users.noreply.github.com>
2017-09-23 09:02:22 -0400

    Create install.js

---
+++ src/app/install.js
@@ -0,0 +1,15 @@
+/*
+new Session(){
+$COMPILER?obj{
+	$COMPILER1?name |	Cc
+	$LANG?obj >> LANG1?name | aQJS	
+	$Cc? >> $LANG? >> $if?nil { %.parse{$OBJECT?obj{
+	<%!!pcdata>
+		ig >> $OBJECT.parse << ig
+		
+	<pcdata!!%>
+	}} }
+
+	}
+};
+*/
